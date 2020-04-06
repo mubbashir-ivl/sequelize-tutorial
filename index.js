924a3ea847a2fc371feeb868052058c384be79f3
mubbashir-ivl <mubbasher.ahmed@indusvalleylabs.com>
2020-04-06 14:14:52 +0500

    update - codes for fetching associations

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const models = require('./models');
 const Op = models.Sequelize.Op;
 
 // Get Addresses
-console.log('Addresses')
-models.Address.findAll()
-  .then(addresses => console.log(addresses))
-  .catch(error => console.log("ERR: " + error));
+// console.log('Addresses')
+// models.Address.findAll()
+//   .then(addresses => console.log(addresses))
+//   .catch(error => console.log("ERR: " + error));
 
 // Get Orders
 console.log('Orders')
-models.Order.findAll()
-  .then(orders => console.log(orders))
+models.Order.findAll({
+  include: [{
+    model: models.Address,
+    as: 'shippingAddress'
+  },{
+    model: models.Address,
+    as: 'billingAddress'
+  }],
+  where: {
+    id: 1
+  }
+})
+  .then(order => console.log(order))
   .catch(error => console.log("ERR: " + error));
